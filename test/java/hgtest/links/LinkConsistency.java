e17e10e0e5b35e6def677c91014804bdcc4d2c4e
borislav.iordanov <borislav.iordanov@43784a7f-e236-0410-9472-c78ae7dffccf>
2008-03-21 23:18:58 +0000

    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@198 43784a7f-e236-0410-9472-c78ae7dffccf

--- test/java/hgtest/links/LinkConsistency.java
+++ test/java/hgtest/links/LinkConsistency.java
@@ -16,6 +16,7 @@ public class LinkConsistency
 	private List<HGPersistentHandle> failed = new ArrayList<HGPersistentHandle>();
 	private int stopAfter = 1;
 	private boolean ignoreCache = false;
+	private boolean ignoreMissing = false;
 	
 	public LinkConsistency(HyperGraph graph)
 	{
@@ -52,6 +53,31 @@ public class LinkConsistency
 	{
 		this.ignoreCache = ignoreCache;
 	}
+	
+	public HyperGraph getGraph()
+	{
+		return graph;
+	}
+
+	public void setGraph(HyperGraph graph)
+	{
+		this.graph = graph;
+	}
+
+	public boolean isIgnoreMissing()
+	{
+		return ignoreMissing;
+	}
+
+	public void setIgnoreMissing(boolean ignoreMissing)
+	{
+		this.ignoreMissing = ignoreMissing;
+	}
+
+	public List<HGPersistentHandle> getFailed()
+	{
+		return failed;
+	}
 
 	/**
 	 * <p>Return true if 'link' is a member of the incidence set of 'target' and
@@ -63,9 +89,38 @@ public class LinkConsistency
 		if (!ignoreCache && graph.isIncidenceSetLoaded(target))
 			is = graph.getIncidenceSet(target);
 		else
+		{
 			is = graph.getStore().getIncidenceSet(target);
+			if (is == null && !ignoreMissing)
+				throw new TestException("The incidence set of atom " + target + " is not available from the HGStore.");
+		}
 		for (HGHandle h : is)
 			if (h.equals(link))
+				return true;
+		return false;
+	}
+	
+	public boolean isLinkMember(HGPersistentHandle link, HGPersistentHandle target)
+	{		
+		if (ignoreCache || !graph.isLoaded(link))
+		{
+			HGPersistentHandle [] layout = graph.getStore().getLink(link);
+			if (layout == null && !ignoreMissing)
+				throw new TestException("The atom handle " + link + 
+						" is refered from incidence set of " + target + 
+						" is not available from the HGStore.");
+			for (int i = 2; i < layout.length; i++)
+				if (layout[i].equals(target))
+					return true;
+			return false;
+		}
+		Object x = graph.get(link);
+		if (! (x instanceof HGLink))
+			throw new TestException("Atom " + link + 
+					" is not a link, but it is a member of the incidence set of " + target);
+		HGLink l = (HGLink)x;
+		for (int i = 0; i < l.getArity(); i++)
+			if (l.getTargetAt(i).equals(target))
 				return true;
 		return false;
 	}
@@ -79,7 +134,7 @@ public class LinkConsistency
 		if (ignoreCache || !graph.isLoaded(link))
 		{
 			HGPersistentHandle [] layout = graph.getStore().getLink(link);
-			if (layout == null)
+			if (layout == null && !ignoreMissing)
 				throw new TestException("The atom handle " + link + " is not available from the HGStore.");
 			for (int i = 2; i < layout.length; i++)
 				if (!isIncidenceSetMember(link, layout[i]))
@@ -98,17 +153,36 @@ public class LinkConsistency
 		return true;		
 	}
 	
+	public boolean isIncidenceSetConsistent(HGPersistentHandle atom)
+	{
+		HGHandle [] is = null;
+		if (ignoreCache || !graph.isIncidenceSetLoaded(atom))
+			is = graph.getStore().getIncidenceSet(atom);
+		else
+			is = graph.getIncidenceSet(atom);
+		if (is == null)
+			return true;
+		for (HGHandle h : is)
+			if (!isLinkMember(graph.getPersistentHandle(h), atom))
+				return false;
+		return true;
+	}
+	
 	public void checkConsistency()
 	{
 		failed.clear();
+		int total = 0;
 		HGSearchResult<HGPersistentHandle> rs = graph.find(cond);
 		try
 		{
 			while (rs.hasNext() && failed.size() < stopAfter)
 			{
-				if (!isLinkConsistent(rs.next()))
+				total++;
+				rs.next();
+				if (!isLinkConsistent(rs.current()) || !isIncidenceSetConsistent(rs.current()))
 					failed.add(rs.current());
 			}
+			System.out.println("Total tested: " + total);
 		}
 		finally
 		{
