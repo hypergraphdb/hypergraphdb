aab86e94611aaeb087aa71344e8f0585512d45f8
borislav.iordanov <borislav.iordanov@43784a7f-e236-0410-9472-c78ae7dffccf>
2008-09-03 13:45:23 +0000

    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@386 43784a7f-e236-0410-9472-c78ae7dffccf

---
+++ core/src/java/org/hypergraphdb/cache/SimpleCache.java
@@ -0,0 +1,75 @@
+package org.hypergraphdb.cache;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import org.hypergraphdb.util.RefResolver;
+
+public class SimpleCache<Key, Value> implements HGCache<Key, Value>
+{	
+	ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+	private RefResolver<Key, Value> resolver;
+	private Map<Key,Value> map = new HashMap<Key, Value>();
+		
+	public SimpleCache()
+	{
+	}
+	
+	public Value get(Key key)
+	{
+		lock.readLock().lock();
+		Value v = map.get(key);
+		lock.readLock().unlock();
+		if (v == null)
+		{
+			v = resolver.resolve(key);
+			lock.writeLock().lock();
+			map.put(key, v);
+			lock.writeLock().unlock();
+			return v;
+		}
+		else
+			return v;
+	}
+
+	public Value getIfLoaded(Key key)
+	{
+		lock.readLock().lock();
+		Value v = map.get(key);
+		lock.readLock().unlock();
+		return v;
+	}
+	
+	public boolean isLoaded(Key key)
+	{
+		lock.readLock().lock();
+		boolean b = map.containsKey(key);
+		lock.readLock().unlock();
+		return b;
+	}
+	
+	public void remove(Key key)
+	{
+		lock.writeLock().lock();
+		map.remove(key);
+		lock.writeLock().unlock();
+	}
+	
+	public RefResolver<Key, Value> getResolver()
+	{
+		return resolver;
+	}
+
+	public void setResolver(RefResolver<Key, Value> resolver)
+	{
+		this.resolver = resolver;
+	}
+	
+	public void clear()
+	{
+		lock.writeLock().lock();
+		map.clear();
+		lock.writeLock().unlock();
+	}
+
+}
