0f9dfc76e47fe8f957f289802a88935cca743c9f
borislav.iordanov <borislav.iordanov@43784a7f-e236-0410-9472-c78ae7dffccf>
2008-10-05 21:12:52 +0000

    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@398 43784a7f-e236-0410-9472-c78ae7dffccf

---
+++ core/src/java/org/hypergraphdb/util/HGLock.java
@@ -0,0 +1,55 @@
+package org.hypergraphdb.util;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import org.hypergraphdb.HyperGraph;
+import org.hypergraphdb.transaction.BDBTxLock;
+
+/**
+ * 
+ * <p>
+ * An implementation of <code>ReadWriteLock</code> that will use the currently
+ * active database transaction if there is one (through <code>BDBTxLock</code>) 
+ * or a default <code>ReentrantReadWriteLock</code> if there is no current transaction.
+ * The implementation is useful for runtime data structures that need protection
+ * from concurrent access and that may or may not participate in a database
+ * transaction. 
+ * </p>
+ *
+ * @author Borislav Iordanov
+ *
+ */
+public class HGLock implements ReadWriteLock
+{
+	private ReentrantReadWriteLock defaultLock = null;
+	private BDBTxLock txLock = null;
+	
+	ReadWriteLock getApplicableLock()
+	{
+		if (txLock.getGraph().getTransactionManager().getContext().getCurrent() == null)
+		{
+			if (defaultLock == null)
+				defaultLock = new ReentrantReadWriteLock();
+			return defaultLock;
+		}
+		else
+			return txLock;
+	}
+	
+	public HGLock(HyperGraph graph, byte [] objectId)
+	{
+		txLock = new BDBTxLock(graph, objectId);
+	}
+	
+	public Lock readLock()
+	{
+		return getApplicableLock().readLock();
+	}
+
+	public Lock writeLock()
+	{
+		return getApplicableLock().writeLock();
+	}
+}
