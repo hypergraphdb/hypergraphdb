707bd0c8d56cc4bb139d2a04879483000b491962
borislav.iordanov <borislav.iordanov@43784a7f-e236-0410-9472-c78ae7dffccf>
2009-04-01 21:42:41 +0000

    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@543 43784a7f-e236-0410-9472-c78ae7dffccf

---
+++ core/src/java/org/hypergraphdb/peer/bootstrap/CACTBootstrap.java
@@ -0,0 +1,18 @@
+package org.hypergraphdb.peer.bootstrap;
+
+import java.util.Map;
+
+import org.hypergraphdb.peer.BootstrapPeer;
+import org.hypergraphdb.peer.HyperGraphPeer;
+import org.hypergraphdb.peer.cact.DefineAtom;
+import org.hypergraphdb.peer.cact.GetClassForType;
+
+public class CACTBootstrap implements BootstrapPeer
+{
+    @Override
+    public void bootstrap(HyperGraphPeer peer, Map<String, Object> config)
+    {
+        peer.getActivityManager().registerActivityType(GetClassForType.TYPENAME, GetClassForType.class);
+        peer.getActivityManager().registerActivityType(DefineAtom.TYPENAME, DefineAtom.class);
+    }
+}
