346478e736d1d880a2c86a6518f66d8b7c08d844
borislav.iordanov <borislav.iordanov@43784a7f-e236-0410-9472-c78ae7dffccf>
2010-05-17 21:21:51 +0000

    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@1040 43784a7f-e236-0410-9472-c78ae7dffccf

--- test/java/hgtest/tx/TypingTxTests.java
+++ test/java/hgtest/tx/TypingTxTests.java
@@ -1,5 +1,11 @@
 package hgtest.tx;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import org.hypergraphdb.HGHandle;
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
@@ -23,5 +29,46 @@ public class TypingTxTests extends HGTestBase
         reopenDb();
         h = graph.getTypeSystem().getTypeHandleIfDefined(SimpleBean.class);
         assertNull(h);        
+    }
+    
+    @Test(invocationCount=1)
+    public void testConcurrentTypeAdd()
+    {
+        ExecutorService pool = Executors.newFixedThreadPool(2);
+        Callable<HGHandle> op = new Callable<HGHandle>()
+        {
+            public HGHandle call()
+            {
+                return graph.getTypeSystem().getTypeHandle(SimpleBean.class);
+            }
+        };
+        Future<HGHandle> f1 = pool.submit(op);
+        Future<HGHandle> f2 = pool.submit(op);
+        try
+        {
+            assertEquals(f1.get(), f2.get());
+        }
+        catch (InterruptedException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (ExecutionException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                graph.remove(f1.get());
+                graph.remove(f2.get());
+            }
+            catch (Throwable e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 }
