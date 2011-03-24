2fabf196133f1658f58f8e28c6283d75fda9c817
borislav.iordanov <borislav.iordanov@43784a7f-e236-0410-9472-c78ae7dffccf>
2011-03-24 17:07:34 +0000

    HyperNode testing and some older uncommitted stuff
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@1338 43784a7f-e236-0410-9472-c78ae7dffccf

--- test/java/hgtest/beans/ComplexBean.java
+++ test/java/hgtest/beans/ComplexBean.java
@@ -40,5 +40,61 @@ public class ComplexBean
 	public void setRemovedNested(SimpleBean removedNested)
 	{
 		this.removedNested = removedNested;
-	}	
+	}
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((removedField == null) ? 0 : removedField.hashCode());
+        result = prime * result
+                + ((removedNested == null) ? 0 : removedNested.hashCode());
+        result = prime * result
+                + ((stableField == null) ? 0 : stableField.hashCode());
+        result = prime * result
+                + ((stableNested == null) ? 0 : stableNested.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ComplexBean other = (ComplexBean) obj;
+        if (removedField == null)
+        {
+            if (other.removedField != null)
+                return false;
+        }
+        else if (!removedField.equals(other.removedField))
+            return false;
+        if (removedNested == null)
+        {
+            if (other.removedNested != null)
+                return false;
+        }
+        else if (!removedNested.equals(other.removedNested))
+            return false;
+        if (stableField == null)
+        {
+            if (other.stableField != null)
+                return false;
+        }
+        else if (!stableField.equals(other.stableField))
+            return false;
+        if (stableNested == null)
+        {
+            if (other.stableNested != null)
+                return false;
+        }
+        else if (!stableNested.equals(other.stableNested))
+            return false;
+        return true;
+    }
+	
 }
