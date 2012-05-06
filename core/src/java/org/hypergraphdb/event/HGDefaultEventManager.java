5c11cb6c5b5056ddb38d549ac261cc1e6ec54296
alpic80@gmail.com <alpic80@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2012-05-06 23:25:02 +0000

    some lazy loading optimizations
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2324 43784a7f-e236-0410-9472-c78ae7dffccf

--- core/src/java/org/hypergraphdb/event/HGDefaultEventManager.java
+++ core/src/java/org/hypergraphdb/event/HGDefaultEventManager.java
@@ -14,52 +14,65 @@ public class HGDefaultEventManager implements HGEventManager
     private HyperGraph graph;
 	private Map<Class<?>, List<HGListener>> listenerMap = null;
 	
-	private List<HGListener> getListeners(Class<?> eventType)
+	private List<HGListener> getListeners(Class<?> eventType, boolean addIfMissing)
 	{
-        List<HGListener> listeners = listenerMap.get(eventType);
-        if (listeners == null)
-        {
-            listeners = new TxList<HGListener>(graph.getTransactionManager());
-            listenerMap.put(eventType, listeners);          
-        }
-        return listeners;
-	}
-	
-	private void init()
-	{
-	    listenerMap = new TxMap<Class<?>, List<HGListener>>(graph.getTransactionManager(), 
-                new HashMap<Class<?>, VBox<List<HGListener>>>());	    		
+			if (listenerMap == null) {
+				if (addIfMissing) {
+					initListenerMap();
+				}
+				else {
+					return null;
+				}
+			}
+			
+      List<HGListener> listeners = listenerMap.get(eventType);
+      if (listeners == null && addIfMissing)
+      {
+          listeners = new TxList<HGListener>(graph.getTransactionManager());
+          listenerMap.put(eventType, listeners);          
+      }
+      return listeners;
 	}
 	
 	public HGDefaultEventManager() { }
+	
 	public HGDefaultEventManager(HyperGraph graph)
 	{
 	    this.graph = graph;
-	    init();
+	}
+	
+	public void initListenerMap()
+	{
+	    listenerMap = new TxMap<Class<?>, List<HGListener>>(graph.getTransactionManager(), 
+	                                                        new HashMap<Class<?>, VBox<List<HGListener>>>());	    
 	}
 	
 	public <T extends HGEvent> void addListener(Class<T> eventType, HGListener listener)
 	{
-		getListeners(eventType).add(listener);
+		getListeners(eventType, true).add(listener);
 	}
 	
 	public <T extends HGEvent> void removeListener(Class<T> eventType, HGListener listener)
 	{
-		getListeners(eventType).remove(listener);
+		List<HGListener> listeners = getListeners(eventType, false);
+		if (listeners != null) {
+			listeners.remove(listener);
+		}
 	}
 	
-	public  HGListener.Result dispatch(HyperGraph graph, HGEvent event)
+	public  HGListener.Result dispatch(HyperGraph hg, HGEvent event)
 	{
-	    if (listenerMap.isEmpty()) // avoid looping through the class hierarchy cause it's expensive
-	        return HGListener.Result.ok;
-		for (Class<?> clazz = event.getClass(); clazz != null && HGEvent.class != clazz; clazz = clazz.getSuperclass())
+    if (listenerMap == null || listenerMap.isEmpty()) // avoid looping through the class hierarchy cause it's expensive
+        return HGListener.Result.ok;
+
+    for (Class<?> clazz = event.getClass(); clazz != null && HGEvent.class != clazz; clazz = clazz.getSuperclass())
 		{
 			List<HGListener> listeners = listenerMap.get(clazz);
 			if (listeners == null)
 				continue;
 			for (HGListener l : listeners)
 				// type safety warning OK, we are explicitly passing a correctly typed event.
-				switch (l.handle(graph, event)) 
+				switch (l.handle(hg, event)) 
 				{
 					case ok: continue;
 					case cancel: return HGListener.Result.cancel;
@@ -71,13 +84,13 @@ public class HGDefaultEventManager implements HGEventManager
 	
 	public void clear()
 	{
-		listenerMap.clear();
+		if (listenerMap != null) 
+			listenerMap.clear();
 	}
 
 	public void setHyperGraph(HyperGraph graph)
 	{
 		this.graph = graph;
-		init();
 	}
 	
 	public HyperGraph getHyperGraph()
