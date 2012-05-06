ac8ec6e6dada08bcaf98c73700e0a42abd84e078
alpic80@gmail.com <alpic80@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2012-05-06 23:32:55 +0000

    expanded coverage to unsigned
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2332 43784a7f-e236-0410-9472-c78ae7dffccf

--- core/src/java/org/hypergraphdb/storage/BAUtils.java
+++ core/src/java/org/hypergraphdb/storage/BAUtils.java
@@ -17,51 +17,69 @@ package org.hypergraphdb.storage;
  */
 public class BAUtils 
 {
-    public static long readLong(byte [] bytes, int offset)
-    {
-        return new Long((((long)bytes[offset] << 56) +
-                ((long)(bytes[offset + 1] & 255) << 48) +
-                ((long)(bytes[offset + 2] & 255) << 40) +
-                ((long)(bytes[offset + 3] & 255) << 32) +
-                ((long)(bytes[offset + 4] & 255) << 24) +
-                ((bytes[offset + 5] & 255) << 16) + 
-                ((bytes[offset + 6] & 255) <<  8) + 
-                ((bytes[offset + 7] & 255) <<  0))); 
-    }
+  public static long readLong(byte [] data, int offset)
+  {
+  	return readUnsignedLong(data, offset) ^ 0x8000000000000000L;
+  }
+  
+  private static long readUnsignedLong(byte [] data, int offset)
+  {
+    return (((long)(data[offset] & 255) << 56) +
+            ((long)(data[offset + 1] & 255) << 48) +
+            ((long)(data[offset + 2] & 255) << 40) +
+            ((long)(data[offset + 3] & 255) << 32) +
+            ((long)(data[offset + 4] & 255) << 24) +
+            ((long)(data[offset + 5] & 255) << 16) + 
+            ((long)(data[offset + 6] & 255) <<  8) + 
+            ((long)(data[offset + 7] & 255) <<  0)); 
+  }
+  
+
+  public static void writeLong(long v, byte [] data, int offset)
+  {
+    writeUnsignedLong(v ^ 0x8000000000000000L, data, offset);
+  }
+  
+  public static void writeUnsignedLong(long v, byte [] data, int offset)
+  {
+    data[offset] = (byte) ((v >>> 56)); 
+    data[offset + 1] = (byte) ((v >>> 48));
+    data[offset + 2] = (byte) ((v >>> 40)); 
+    data[offset + 3] = (byte) ((v >>> 32));
+    data[offset + 4] = (byte) ((v >>> 24)); 
+    data[offset + 5] = (byte) ((v >>> 16));
+    data[offset + 6] = (byte) ((v >>> 8)); 
+    data[offset + 7] = (byte) ((v >>> 0));
+  }
     
-    public static void writeLong(long v, byte [] data, int offset)
-    {
-        data[offset] = (byte) ((v >>> 56)); 
-        data[offset + 1] = (byte) ((v >>> 48));
-        data[offset + 2] = (byte) ((v >>> 40)); 
-        data[offset + 3] = (byte) ((v >>> 32));
-        data[offset + 4] = (byte) ((v >>> 24)); 
-        data[offset + 5] = (byte) ((v >>> 16));
-        data[offset + 6] = (byte) ((v >>> 8)); 
-        data[offset + 7] = (byte) ((v >>> 0));
-    }
-    
-	public static int readInt(byte [] buffer, int offset)
+	public static int readInt(byte[] data, int offset)
 	{
-        int ch1 = buffer[offset];
-        int ch2 = buffer[offset + 1];
-        int ch3 = buffer[offset + 2];
-        int ch4 = buffer[offset + 3];
-        return ((ch1 & 0xFF) << 24)
-	      | ((ch2 & 0xFF) << 16)
-	      | ((ch3 & 0xFF) << 8)
-	      | (ch4 & 0xFF);
+    return (int) (readUnsignedInt(data, offset) ^ 0x80000000);
 	}
 	
-	public static void writeInt(int c, byte [] buffer, int offset)
+	public static long readUnsignedInt(byte[] data, int offset)
 	{
-		buffer[offset + 0] = (byte) ((c >>> 24) & 0xFF);
-        buffer[offset + 1] = (byte) ((c >>> 16) & 0xFF);
-        buffer[offset + 2] = (byte) ((c >>> 8) & 0xFF);
-        buffer[offset + 3] = (byte) ((c >>> 0) & 0xFF);		
+    long c1 = (data[offset] & 0xFF);
+    long c2 = (data[offset + 1] & 0xFF);
+    long c3 = (data[offset + 2] & 0xFF);
+    long c4 = (data[offset + 3] & 0xFF);
+    return ((c1 << 24) | (c2 << 16) | (c3 << 8) | c4);
 	}
 	
-	public static boolean eq(byte [] left, int leftPos, byte [] right, int rightPos, int size)
+	public static void writeInt(int val, byte[] data, int offset)
+	{
+    writeUnsignedInt(val ^ 0x80000000, data, offset);
+	}
+	
+	public static void writeUnsignedInt(long c, byte[] data, int offset)
+	{
+		data[offset + 0] = (byte) ((c >>> 24) & 0xFF);
+		data[offset + 1] = (byte) ((c >>> 16) & 0xFF);
+		data[offset + 2] = (byte) ((c >>> 8) & 0xFF);
+		data[offset + 3] = (byte) ((c >>> 0) & 0xFF);
+	}
+	
+	public static boolean eq(byte[] left, int leftPos, byte[] right, int rightPos, int size)
 	{
 		int i = leftPos, j = rightPos;
 		if (leftPos + size > left.length)
@@ -77,7 +95,7 @@ public class BAUtils
 		return true;
 	}
 	
-	public static int compare(byte [] left, int leftPos, byte [] right, int rightPos, int max)
+	public static int compare(byte[] left, int leftPos, byte[] right, int rightPos, int max)
 	{
 		int maxLeft = leftPos + max;
 		int maxRight = rightPos + max;
