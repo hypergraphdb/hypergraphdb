05eba21635c4126b44d2d387f30b037885b2b09e
alpic80@gmail.com <alpic80@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2012-09-05 12:19:44 +0000

    Convert spaces to tabs and remove trailing spaces
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2555 43784a7f-e236-0410-9472-c78ae7dffccf

--- core/src/java/org/hypergraphdb/HGSearchable.java
+++ core/src/java/org/hypergraphdb/HGSearchable.java
@@ -1,9 +1,9 @@
-/* 
- * This file is part of the HyperGraphDB source distribution. This is copyrighted 
- * software. For permitted uses, licensing options and redistribution, please see  
- * the LicensingInformation file at the root level of the distribution.  
- * 
- * Copyright (c) 2005-2010 Kobrix Software, Inc.  All rights reserved. 
+/*
+ * This file is part of the HyperGraphDB source distribution. This is copyrighted
+ * software. For permitted uses, licensing options and redistribution, please see
+ * the LicensingInformation file at the root level of the distribution.
+ *
+ * Copyright (c) 2005-2010 Kobrix Software, Inc.  All rights reserved.
  */
 package org.hypergraphdb;
 
@@ -13,19 +13,19 @@ package org.hypergraphdb;
  * The <code>HGSearchable</code> interface specifies that an object can be searched by a key.
  * Things <code>HGSearchable</code> in HyperGraph are indexes, certain types and the like.
  * </p>
- * 
+ *
  * @author Borislav Iordanov
  */
 public interface HGSearchable<KeyType, ValueType>
 {
-    /**
-     * <p>
-     * Returns a <code>HGSearchResult</code> over all values matching a key in the 
-     * searched entity.</p>
-     * 
-     * <p>If there are no matches, the method should return <code>HGSearchResult.EMPTY</code>. The
-     * method will never return a <code>null</code>. A <code>HGException</code> may
-     * be thrown in exceptional situations.</p>
-     */
-    HGSearchResult<ValueType> find(KeyType key);
+		/**
+		 * <p>
+		 * Returns a <code>HGSearchResult</code> over all values matching a key in the
+		 * searched entity.</p>
+		 *
+		 * <p>If there are no matches, the method should return <code>HGSearchResult.EMPTY</code>. The
+		 * method will never return a <code>null</code>. A <code>HGException</code> may
+		 * be thrown in exceptional situations.</p>
+		 */
+		HGSearchResult<ValueType> find(KeyType key);
 }
--- core/src/java/org/hypergraphdb/HGSortIndex.java
+++ core/src/java/org/hypergraphdb/HGSortIndex.java
@@ -1,9 +1,9 @@
-/* 
- * This file is part of the HyperGraphDB source distribution. This is copyrighted 
- * software. For permitted uses, licensing options and redistribution, please see  
- * the LicensingInformation file at the root level of the distribution.  
- * 
- * Copyright (c) 2005-2010 Kobrix Software, Inc.  All rights reserved. 
+/*
+ * This file is part of the HyperGraphDB source distribution. This is copyrighted
+ * software. For permitted uses, licensing options and redistribution, please see
+ * the LicensingInformation file at the root level of the distribution.
+ *
+ * Copyright (c) 2005-2010 Kobrix Software, Inc.  All rights reserved.
  */
 package org.hypergraphdb;
 
@@ -13,21 +13,21 @@ package org.hypergraphdb;
  * keys in an order relation. It is therefore an <code>HGOrderedSearchable</code>
  * entity.
  * </p>
- * 
+ *
  * <p>
  * Taking advantage of the new Java 1.5 feature allowing overriding methods to
  * further specialize on the return type (i.e. allowing contravariant return types),
- * all lookup methods of the super-interfaces are redeclared to return a 
- * <code>HGRandomAccessResult</code>. 
+ * all lookup methods of the super-interfaces are redeclared to return a
+ * <code>HGRandomAccessResult</code>.
  * </p>
- * 
+ *
  * @author Borislav Iordanov
  */
-public interface HGSortIndex<KeyType, ValueType> extends HGIndex<KeyType, ValueType>, 
+public interface HGSortIndex<KeyType, ValueType> extends HGIndex<KeyType, ValueType>,
 														 HGOrderedSearchable<KeyType, ValueType>
 {
 	HGSearchResult<ValueType> findLT(KeyType key);
 	HGSearchResult<ValueType> findGT(KeyType key);
 	HGSearchResult<ValueType> findLTE(KeyType key);
-	HGSearchResult<ValueType> findGTE(KeyType key);	
+	HGSearchResult<ValueType> findGTE(KeyType key);
 }
