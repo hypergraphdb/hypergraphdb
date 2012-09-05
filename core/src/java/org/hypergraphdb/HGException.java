a04097641e5ca735653d5bfc11f3bc2c5566f04f
alpic80@gmail.com <alpic80@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2012-09-05 12:13:55 +0000

    Convert spaces to tabs and remove trailing spaces
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2553 43784a7f-e236-0410-9472-c78ae7dffccf

--- core/src/java/org/hypergraphdb/HGException.java
+++ core/src/java/org/hypergraphdb/HGException.java
@@ -1,18 +1,18 @@
-/* 
- * This file is part of the HyperGraphDB source distribution. This is copyrighted 
- * software. For permitted uses, licensing options and redistribution, please see  
- * the LicensingInformation file at the root level of the distribution.  
- * 
- * Copyright (c) 2005-2010 Kobrix Software, Inc.  All rights reserved. 
+/*
+ * This file is part of the HyperGraphDB source distribution. This is copyrighted
+ * software. For permitted uses, licensing options and redistribution, please see
+ * the LicensingInformation file at the root level of the distribution.
+ *
+ * Copyright (c) 2005-2010 Kobrix Software, Inc.  All rights reserved.
  */
 package org.hypergraphdb;
 
 /**
- * 
+ *
  * <p>
  * A <code>HGException</code> is thrown by HyperGraph code anywhere an abnormal
  * situation, that cannot usually be handled in an obvious way besides
- * debugging, occurs.  
+ * debugging, occurs.
  * </p>
  *
  * @author Borislav Iordanov
@@ -20,20 +20,20 @@ package org.hypergraphdb;
  */
 public class HGException extends RuntimeException
 {
-    static final long serialVersionUID = -1;
-    
-    public HGException(String msg)
-    {
-        super(msg);
-    }
-    
-    public HGException(String msg, Throwable cause)
-    {
-        super(msg, cause);
-    }
-    
-    public HGException(Throwable cause)
-    {
-        super(cause);
-    }
+	static final long serialVersionUID = -1;
+
+	public HGException(String msg)
+	{
+		super(msg);
+	}
+
+	public HGException(String msg, Throwable cause)
+	{
+		super(msg, cause);
+	}
+
+	public HGException(Throwable cause)
+	{
+		super(cause);
+	}
 }
--- core/src/java/org/hypergraphdb/HGHandleFactory.java
+++ core/src/java/org/hypergraphdb/HGHandleFactory.java
@@ -1,9 +1,9 @@
-/* 
- * This file is part of the HyperGraphDB source distribution. This is copyrighted 
- * software. For permitted uses, licensing options and redistribution, please see  
- * the LicensingInformation file at the root level of the distribution.  
- * 
- * Copyright (c) 2005-2010 Kobrix Software, Inc.  All rights reserved. 
+/*
+ * This file is part of the HyperGraphDB source distribution. This is copyrighted
+ * software. For permitted uses, licensing options and redistribution, please see
+ * the LicensingInformation file at the root level of the distribution.
+ *
+ * Copyright (c) 2005-2010 Kobrix Software, Inc.  All rights reserved.
  */
 package org.hypergraphdb;
 
@@ -16,74 +16,74 @@ package org.hypergraphdb;
  * that must be unique at least within the current database instance. Certain factories
  * can also guarantee universal uniqueness (e.g. in a distributed environment or across
  * the Internet). Factories are also responsible for creating handle out of their
- * <code>String</code> or <code>byte[]</code> representations. The main requirement that 
+ * <code>String</code> or <code>byte[]</code> representations. The main requirement that
  * a handle factory implementation must observe is that all
  * persistent handles must be of the exact same <code>byte[]</code> size. For example, this
  * rules out the use of URIs as persistent handles.
  * </p>
- * 
+ *
  * @author Borislav Iordanov
  */
 public interface HGHandleFactory
 {
-    /**
-     * <p>Construct and return a new, unique persistent handle. The handle 
-     * is permanently unique. It can be persistent (serialized) and restored without
-     * ever conflicting with another handle. 
-     * </p>
-     */
-    HGPersistentHandle makeHandle();    
-    /**
-     * <p>Construct a persistent handle from its string representation.</p>
-     */
-    HGPersistentHandle makeHandle(String handleAsString);
-    
-    /**
-     * <p>Construct a persistent handle from its byte array representation.</p>
-     * 
-     * @param buffer The byte array holding the handle value.
-     */
-    HGPersistentHandle makeHandle(byte [] buffer);
+	/**
+	 * <p>Construct and return a new, unique persistent handle. The handle
+	 * is permanently unique. It can be persistent (serialized) and restored without
+	 * ever conflicting with another handle.
+	 * </p>
+	 */
+	HGPersistentHandle makeHandle();
+	/**
+	 * <p>Construct a persistent handle from its string representation.</p>
+	 */
+	HGPersistentHandle makeHandle(String handleAsString);
 
-    /**
-     * <p>Construct a persistent handle from its byte array representation where the byte array
-     * is part of a larger buffer and located at a particular offset.</p>
-     * 
-     * @param buffer The byte array holding the handle value.
-     * @param offset The offset within <code>buffer</code> where the handle value starts.
-     */
-    HGPersistentHandle makeHandle(byte [] buffer, int offset);
-    
-    /**
-     * <p>Return the representation of a null persistent handle. A null
-     * handle is a single instance and therefore can be compared for equality by
-     * doing direct Java object reference comparison. A null handle can be 
-     * recorded in storage as any other persistent handle - it refers to no
-     * value. Not that while HyperGraph's type system does not provide 
-     * for <code>null</code> atom values or value projections, null references
-     * to values are permitted and supported through the null 
-     * <code>HGPersistentHandle</code></p>
-     */
-    HGPersistentHandle nullHandle();
-    
-    /**
-     * <p>
-     * The <code>anyHandle</code> is a persistent handle constant that represents
-     * a "don't care" handle during querying and comparison operations. It can be used, for
-     * instance, when defining an <code>OrdererLinkCondition</code> in a query. 
-     * </p>
-     * 
-     * <p>
-     * For efficiency reasons, the <code>equals</code> method of the various implementations of 
-     * the <code>HGHandle</code> interface ignore this constant, even though it would be
-     * more consistent to for <code>anyHandle.equals(x)</code> and <code>x.equals(anyHandle)</code>
-     * to always return true.
-     * </p>
-     */        
-    HGPersistentHandle anyHandle();
-    
-    HGPersistentHandle topTypeHandle();
-    HGPersistentHandle nullTypeHandle();
-    HGPersistentHandle linkTypeHandle();
-    HGPersistentHandle subsumesTypeHandle();
+	/**
+	 * <p>Construct a persistent handle from its byte array representation.</p>
+	 *
+	 * @param buffer The byte array holding the handle value.
+	 */
+	HGPersistentHandle makeHandle(byte [] buffer);
+
+	/**
+	 * <p>Construct a persistent handle from its byte array representation where the byte array
+	 * is part of a larger buffer and located at a particular offset.</p>
+	 *
+	 * @param buffer The byte array holding the handle value.
+	 * @param offset The offset within <code>buffer</code> where the handle value starts.
+	 */
+	HGPersistentHandle makeHandle(byte [] buffer, int offset);
+
+	/**
+	 * <p>Return the representation of a null persistent handle. A null
+	 * handle is a single instance and therefore can be compared for equality by
+	 * doing direct Java object reference comparison. A null handle can be
+	 * recorded in storage as any other persistent handle - it refers to no
+	 * value. Not that while HyperGraph's type system does not provide
+	 * for <code>null</code> atom values or value projections, null references
+	 * to values are permitted and supported through the null
+	 * <code>HGPersistentHandle</code></p>
+	 */
+	HGPersistentHandle nullHandle();
+
+/**
+	 * <p>
+	 * The <code>anyHandle</code> is a persistent handle constant that represents
+	 * a "don't care" handle during querying and comparison operations. It can be used, for
+	 * instance, when defining an <code>OrdererLinkCondition</code> in a query.
+	 * </p>
+	 *
+	 * <p>
+	 * For efficiency reasons, the <code>equals</code> method of the various implementations of
+	 * the <code>HGHandle</code> interface ignore this constant, even though it would be
+	 * more consistent to for <code>anyHandle.equals(x)</code> and <code>x.equals(anyHandle)</code>
+	 * to always return true.
+	 * </p>
+	 */
+	HGPersistentHandle anyHandle();
+
+	HGPersistentHandle topTypeHandle();
+	HGPersistentHandle nullTypeHandle();
+	HGPersistentHandle linkTypeHandle();
+	HGPersistentHandle subsumesTypeHandle();
 }
