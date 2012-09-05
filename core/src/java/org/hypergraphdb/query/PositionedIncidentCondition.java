deb306cadc049eeecb4fd5670e98619a55289387
alpic80@gmail.com <alpic80@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2012-09-05 11:56:37 +0000

    added small validation test
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2546 43784a7f-e236-0410-9472-c78ae7dffccf

--- core/src/java/org/hypergraphdb/query/PositionedIncidentCondition.java
+++ core/src/java/org/hypergraphdb/query/PositionedIncidentCondition.java
@@ -123,6 +123,8 @@ public class PositionedIncidentCondition implements HGQueryCondition, HGAtomPred
 		else
 		{
 			HGPersistentHandle[] A = hg.getStore().getLink(handle.getPersistent());
+			if (A == null || A.length <=2)  //this indicates that we don't have a link 
+				return false;
 			link = new TempLink(A, 2);
 		}
 		
