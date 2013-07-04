0ab492933dffd964b2b789e17be251bbbb453c02
YuriySechko@gmail.com <YuriySechko@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2013-07-04 13:41:56 +0000

    BJEStorageImplementation.getData is covered by test cases
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2869 43784a7f-e236-0410-9472-c78ae7dffccf

--- storage/bdb-je/test/java/hgtest/storage/bje/BJEStorageImplementation_getDataTest.java
+++ storage/bdb-je/test/java/hgtest/storage/bje/BJEStorageImplementation_getDataTest.java
@@ -9,6 +9,7 @@ import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.assertNull;
+import static org.testng.Assert.fail;
 
 /**
  */
@@ -16,12 +17,68 @@ public class BJEStorageImplementation_getDataTest extends
 		BJEStorageImplementationTestBasis
 {
 	@Test
-	public void readDataWhichIsNotStoredUsingGivenHandle() throws Exception
+	public void readDataWhichIsNotStored() throws Exception
 	{
 		startup(1);
 		final HGPersistentHandle handle = new UUIDPersistentHandle();
-		final byte[] retrievedData = storage.getData(handle);
-		assertNull(retrievedData);
+		final byte[] retrieved = storage.getData(handle);
+		assertNull(retrieved);
+		shutdown();
+	}
+
+	@Test
+	public void useNullHandle() throws Exception
+	{
+		startup();
+		try
+		{
+			storage.getData(null);
+		}
+		catch (Exception ex)
+		{
+			assertEquals(ex.getClass(), org.hypergraphdb.HGException.class);
+			assertEquals(ex.getMessage(),
+					"Failed to retrieve link with handle null");
+		}
+		finally
+		{
+			shutdown();
+		}
+	}
+
+	@Test
+	public void readEmptyArray() throws Exception
+	{
+		final byte[] expected = new byte[] {};
+		startup(2);
+		final HGPersistentHandle handle = new UUIDPersistentHandle();
+		storage.store(handle, new byte[] {});
+		final byte[] retrieved = storage.getData(handle);
+		assertEquals(retrieved, expected);
+		shutdown();
+	}
+
+	@Test
+	public void readArrayWhichContainsOneItem() throws Exception
+	{
+		final byte[] expected = new byte[] { 44 };
+		startup(2);
+		final HGPersistentHandle handle = new UUIDPersistentHandle();
+		storage.store(handle, new byte[] { 44 });
+		final byte[] retrieved = storage.getData(handle);
+		assertEquals(retrieved, expected);
+		shutdown();
+	}
+
+	@Test
+	public void readArrayWhichContainsSeveralItems() throws Exception
+	{
+		final byte[] expected = new byte[] { 11, 22, 33 };
+		startup(2);
+		final HGPersistentHandle handle = new UUIDPersistentHandle();
+		storage.store(handle, new byte[] { 11, 22, 33 });
+		final byte[] retrieved = storage.getData(handle);
+		assertEquals(retrieved, expected);
 		shutdown();
 	}
 }
