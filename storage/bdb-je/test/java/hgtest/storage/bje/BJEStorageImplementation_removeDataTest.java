7d50bc6b69dac55e57f55100a90e993a47cd203b
YuriySechko@gmail.com <YuriySechko@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2013-07-04 14:13:20 +0000

    BJEStorageImplementation.removeData is covered by test cases
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2870 43784a7f-e236-0410-9472-c78ae7dffccf

--- storage/bdb-je/test/java/hgtest/storage/bje/BJEStorageImplementation_removeDataTest.java
+++ storage/bdb-je/test/java/hgtest/storage/bje/BJEStorageImplementation_removeDataTest.java
@@ -16,19 +16,67 @@ public class BJEStorageImplementation_removeDataTest extends
 		BJEStorageImplementationTestBasis
 {
 	@Test
-	public void removeDataUsingHandle() throws Exception
+	public void removeArrayWhichContainsSeveralItems() throws Exception
 	{
 		startup(3);
 		final HGPersistentHandle handle = new UUIDPersistentHandle();
 		storage.store(handle, new byte[] { 1, 2, 3 });
 		storage.removeData(handle);
-		final byte[] retrievedData = storage.getData(handle);
-		assertNull(retrievedData);
+		final byte[] retrieved = storage.getData(handle);
+		assertNull(retrieved);
 		shutdown();
 	}
 
 	@Test
-	public void removeDataUsingNullHandle() throws Exception
+	public void removeEmptyArray() throws Exception
+	{
+		startup(3);
+		final HGPersistentHandle handle = new UUIDPersistentHandle();
+		storage.store(handle, new byte[] {});
+		storage.removeData(handle);
+		final byte[] retrieved = storage.getData(handle);
+		assertNull(retrieved);
+		shutdown();
+	}
+
+	@Test
+	public void removeArrayWhichContainsOneItem() throws Exception
+	{
+		startup(3);
+		final HGPersistentHandle handle = new UUIDPersistentHandle();
+		storage.store(handle, new byte[] { 2 });
+		storage.removeData(handle);
+		final byte[] retrieved = storage.getData(handle);
+		assertNull(retrieved);
+		shutdown();
+	}
+
+	@Test
+	public void removeDataWhichIsNotStored() throws Exception
+	{
+		startup(2);
+		final HGPersistentHandle handle = new UUIDPersistentHandle();
+		storage.removeData(handle);
+		final byte[] retrieved = storage.getData(handle);
+		assertNull(retrieved);
+		shutdown();
+	}
+
+	@Test
+	public void removeDataUsingAnotherHandle() throws Exception
+	{
+		final byte[] expected = new byte[] { 11, 22, 33 };
+		startup(3);
+		final HGPersistentHandle handle = new UUIDPersistentHandle();
+		final HGPersistentHandle anotherHandle = new UUIDPersistentHandle();
+		storage.store(handle, new byte[] { 11, 22, 33 });
+		storage.removeData(anotherHandle);
+		final byte[] retrieved = storage.getData(handle);
+		assertEquals(retrieved, expected);
+	}
+
+	@Test
+	public void useNullHandle() throws Exception
 	{
 		startup();
 		try
@@ -40,6 +88,33 @@ public class BJEStorageImplementation_removeDataTest extends
 			assertEquals(ex.getClass(), NullPointerException.class);
 			assertEquals(ex.getMessage(),
 					"HGStore.remove called with a null handle.");
+		}
+		finally
+		{
+			shutdown();
+		}
+	}
+
+	@Test
+	public void exceptionWhileRemovingData() throws Exception
+	{
+		mockStore();
+		mockConfiguration(2);
+		mockStoreToThrowException();
+		replay();
+		storage.startup(store, configuration);
+		final HGPersistentHandle handle = new UUIDPersistentHandle();
+		try
+		{
+			storage.removeData(handle);
+		}
+		catch (Exception ex)
+		{
+			assertEquals(ex.getClass(), org.hypergraphdb.HGException.class);
+			final String expectedMessage = String
+					.format("Failed to remove value with handle %s: java.lang.IllegalStateException: Throw exception in test case.",
+							handle);
+			assertEquals(ex.getMessage(), expectedMessage);
 		}
 		finally
 		{
