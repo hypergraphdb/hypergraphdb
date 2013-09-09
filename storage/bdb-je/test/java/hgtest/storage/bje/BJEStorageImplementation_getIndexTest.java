b5e6e09cbf094fee23c58e9db99bf666e435cc9f
YuriySechko@gmail.com <YuriySechko@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2013-09-09 17:50:30 +0000

    some test cases for BJEStorageImplementation.getIndex()
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2900 43784a7f-e236-0410-9472-c78ae7dffccf

---
+++ storage/bdb-je/test/java/hgtest/storage/bje/BJEStorageImplementation_getIndexTest.java
@@ -0,0 +1,68 @@
+package hgtest.storage.bje;
+
+import org.hypergraphdb.HGIndex;
+import org.hypergraphdb.storage.ByteArrayConverter;
+import org.testng.annotations.Test;
+
+import java.util.Comparator;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
+
+/**
+ */
+public class BJEStorageImplementation_getIndexTest extends
+		BJEStorageImplementationTestBasis
+{
+	@Test
+	public void convertersAndComparatorsAreNull() throws Exception
+	{
+		startup(1);
+		final String indexName = "sampleIndex";
+		final ByteArrayConverter<Integer> keyConverter = null;
+		final ByteArrayConverter<String> valueConverter = null;
+		final Comparator<?> comparator = null;
+		final boolean bidirectional = true;
+		final boolean createIfNecessary = true;
+		final HGIndex<Integer, String> createdIndex = storage.getIndex(
+				indexName, keyConverter, valueConverter, comparator,
+				bidirectional, createIfNecessary);
+		assertNotNull(createdIndex);
+		shutdown();
+	}
+
+	@Test
+	public void bidirectionalFlagIsSetToFalse() throws Exception
+	{
+		startup(1);
+		final boolean bidirectional = false;
+		final HGIndex<Integer, String> createdIndex = storage.getIndex(
+				"sampleIndex", null, null, null, bidirectional, true);
+		assertNotNull(createdIndex);
+		shutdown();
+	}
+
+	@Test
+	public void createIfNecessaryFlagIsSetToFalse() throws Exception
+	{
+		startup();
+		final boolean createIfNecessary = false;
+		final HGIndex<Object, Object> createdIndex = storage.getIndex(
+				"sampleIndex", null, null, null, true, createIfNecessary);
+		assertNull(createdIndex);
+		shutdown();
+	}
+
+	@Test
+	public void indexWithTheSameNameAlreadyExists() throws Exception
+	{
+		startup(1);
+		final String indexName = "sample index";
+		final HGIndex<Object, Object> firstIndex = storage.getIndex(indexName, null, null, null, true, true);
+		final HGIndex<Object, Object> secondIndex = storage.getIndex(indexName,
+				null, null, null, true, true);
+        assertEquals(firstIndex, secondIndex);
+		shutdown();
+	}
+}
