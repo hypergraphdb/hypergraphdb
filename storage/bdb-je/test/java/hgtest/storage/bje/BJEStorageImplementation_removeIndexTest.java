2a63937bd1cd63afdcb0c37e228b7ded67926588
YuriySechko@gmail.com <YuriySechko@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2013-09-10 17:24:14 +0000

    method BJEStorageImplementation.removeIndex() is covered by test cases
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2902 43784a7f-e236-0410-9472-c78ae7dffccf

---
+++ storage/bdb-je/test/java/hgtest/storage/bje/BJEStorageImplementation_removeIndexTest.java
@@ -0,0 +1,72 @@
+package hgtest.storage.bje;
+
+import org.hypergraphdb.HGIndex;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
+
+/**
+ */
+public class BJEStorageImplementation_removeIndexTest extends
+		BJEStorageImplementationTestBasis
+{
+	@Test
+	public void indexNameIsNull() throws Exception
+	{
+		startup();
+		final String indexName = null;
+		try
+		{
+			storage.removeIndex(indexName);
+		}
+		catch (Exception occurred)
+		{
+			assertEquals(occurred.getClass(),
+					org.hypergraphdb.HGException.class);
+			assertEquals(
+					occurred.getMessage(),
+					"com.sleepycat.je.DatabaseNotFoundException: (JE 5.0.34) Attempted to remove non-existent database hgstore_idx_null");
+		}
+		finally
+		{
+			shutdown();
+		}
+	}
+
+	@Test
+	public void removeIndexWhichIsNotStored() throws Exception
+	{
+		startup();
+		try
+		{
+			storage.removeIndex("This index does not exist");
+		}
+		catch (Exception occurred)
+		{
+			assertEquals(occurred.getClass(),
+					org.hypergraphdb.HGException.class);
+			assertEquals(
+					occurred.getMessage(),
+					"com.sleepycat.je.DatabaseNotFoundException: (JE 5.0.34) Attempted to remove non-existent database hgstore_idx_This index does not exist");
+		}
+		finally
+		{
+			shutdown();
+		}
+	}
+
+	@Test
+	public void removeIndexWhichExists() throws Exception
+	{
+		startup(1);
+		final String indexName = "sample index";
+		storage.getIndex(indexName,
+				null, null, null, true, true);
+		storage.removeIndex(indexName);
+        final HGIndex<Object, Object> removedIndex = storage.getIndex(indexName, null, null, null, true, false);
+        assertNull(removedIndex);
+        shutdown();
+	}
+}
