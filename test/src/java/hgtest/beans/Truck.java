24cd8196fdfe2bf72699908674572425301b31d9
borislav.iordanov <borislav.iordanov@43784a7f-e236-0410-9472-c78ae7dffccf>
2014-11-15 22:32:13 +0000

    a few test classes and MANIFEST file updates
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2961 43784a7f-e236-0410-9472-c78ae7dffccf

---
+++ test/src/java/hgtest/beans/Truck.java
@@ -0,0 +1,24 @@
+package hgtest.beans;
+
+public class Truck extends Transport
+{
+    int length;
+    int capacity;
+    
+    public int getLength()
+    {
+        return length;
+    }
+    public void setLength(int length)
+    {
+        this.length = length;
+    }
+    public int getCapacity()
+    {
+        return capacity;
+    }
+    public void setCapacity(int capacity)
+    {
+        this.capacity = capacity;
+    }     
+}
