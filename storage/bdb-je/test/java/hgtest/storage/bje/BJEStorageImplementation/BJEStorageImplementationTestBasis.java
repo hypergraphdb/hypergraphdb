bbaa513fa296e7c1679437f1c1727c8d6b7ab9f2
yuriysechko <yuriysechko@43784a7f-e236-0410-9472-c78ae7dffccf>
2014-12-20 12:58:17 +0000

    Javadoc updated
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2966 43784a7f-e236-0410-9472-c78ae7dffccf

--- storage/bdb-je/test/java/hgtest/storage/bje/BJEStorageImplementation/BJEStorageImplementationTestBasis.java
+++ storage/bdb-je/test/java/hgtest/storage/bje/BJEStorageImplementation/BJEStorageImplementationTestBasis.java
@@ -25,15 +25,57 @@ import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.assertNull;
 
 /**
- * This class contains unit tests for {@link BJEStorageImplementation}. For this
- * purpose we use PowerMock + EasyMock test framework (see <a
- * href="http://code.google.com/p/powermock/">PowerMock page on Google Code</a>
- * and <a href="http://easymock.org/">EasyMock home page</a> for details).
- * EasyMock's capabilities are sufficient for interfaces and plain classes. But
- * some classes in hypergraphdb modules are final so we cannot startup it in
- * usual way. PowerMock allows to create mocks even for final classes. So we can
- * test {@link BJEStorageImplementation} in isolation from other environment and
- * in most cases (if required) from other classes.
+ * <p>
+ * This class used as basis for writing test cases for
+ * {@link BJEStorageImplementation}. PowerMock + EasyMock frameworks are used.
+ * (see <a href="http://code.google.com/p/powermock/">PowerMock page on Google
+ * Code</a> and <a href="http://easymock.org/">EasyMock home page</a> for
+ * details). EasyMock's capabilities are sufficient for interfaces and plain
+ * classes. But some classes in hypergraphdb modules are final so we cannot mock
+ * them in usual way. PowerMock allows to create mocks even for final classes.
+ * So we can test {@link BJEStorageImplementation} in isolation from other
+ * environment and in most cases from other classes.
+ * </p>
+ * <p>
+ * Typical test case executes through several phases like initialization,
+ * executing and verification.
+ * </p>
+ * <p>
+ * {@link BJEStorageImplementation} requires two parameters before it can start
+ * functioning: {@link org.hypergraphdb.HGStore} and
+ * {@link org.hypergraphdb.HGConfiguration}. As mentioned above we test
+ * {@link BJEStorageImplementation} in isolation. So the
+ * {@link org.hypergraphdb.HGStore} and {@link org.hypergraphdb.HGConfiguration}
+ * parameters should be mocked. It is done in
+ * {@link BJEStorageImplementationTestBasis#mockStore()} and
+ * {@link BJEStorageImplementationTestBasis#mockConfiguration(int)} methods.
+ * </p>
+ * <p>
+ * The inner code in {@link BJEStorageImplementation} makes several calls to the
+ * {@link org.hypergraphdb.HGStore} and {@link org.hypergraphdb.HGConfiguration}
+ * objects. For {@link org.hypergraphdb.HGStore} only getDatabaseLocation() is
+ * in use.
+ * </p>
+ * <p>
+ * Mocking {@link org.hypergraphdb.HGConfiguration} object is more complicated.
+ * Here we need to specify handle factory and {@code isTransactional} parameter.
+ * In different test cases the number of calls may vary (For example if we test
+ * reading the data we firstly store it and then read it. Each time we
+ * store/read {@link org.hypergraphdb.HGConfiguration#isTransactional()} is
+ * called). So number the of calls is passed using parameter {@code calls}.
+ * </p>
+ * <p>
+ * In some test cases we need to test behavior of
+ * {@link BJEStorageImplementation} when method is not executed properly and
+ * exception somewhere inside is thrown or transaction was not opened etc. For
+ * such test cases can be used specific initialization methods (like
+ * {@link hgtest.storage.bje.BJEStorageImplementation.BJEStorageImplementationTestBasis#startup(int, java.lang.Exception)}
+ * and
+ * {@link hgtest.storage.bje.BJEStorageImplementation.BJEStorageImplementationTestBasis#startupNonTransactional()
+ * )}.
+ * </p>
+ * 
+ * Also this class contains some utility methods.
  * 
  * @author Yuriy Sechko
  */
@@ -44,8 +86,8 @@ public class BJEStorageImplementationTestBasis extends PowerMockTestCase
 	protected static final String HGHANDLEFACTORY_IMPLEMENTATION_CLASS_NAME = "org.hypergraphdb.handle.UUIDHandleFactory";
 
 	// location of temporary directory for tests
-	final protected String testDatabaseLocation = System.getProperty("user.home")
-			+ File.separator + "hgtest.tmp";
+	final protected String testDatabaseLocation = System
+			.getProperty("user.home") + File.separator + "hgtest.tmp";
 
 	// classes which are used by BJEStorageImplementation
 	HGStore store;
@@ -167,16 +209,16 @@ public class BJEStorageImplementationTestBasis extends PowerMockTestCase
 	protected void startupWithAdditionalTransaction(
 			final int transactionManagerCalls) throws Exception
 	{
-        mockConfiguration(2);
-        mockStore();
-        // mock transaction manager
-        final HGTransactionManager transactionManager = new HGTransactionManager(
-                storage.getTransactionFactory());
-        EasyMock.expect(store.getTransactionManager())
-                .andReturn(transactionManager).times(transactionManagerCalls);
-        replay();
-        storage.startup(store, configuration);
-        transactionManager.beginTransaction();
+		mockConfiguration(2);
+		mockStore();
+		// mock transaction manager
+		final HGTransactionManager transactionManager = new HGTransactionManager(
+				storage.getTransactionFactory());
+		EasyMock.expect(store.getTransactionManager())
+				.andReturn(transactionManager).times(transactionManagerCalls);
+		replay();
+		storage.startup(store, configuration);
+		transactionManager.beginTransaction();
 	}
 
 	protected void shutdown() throws Exception
@@ -185,7 +227,8 @@ public class BJEStorageImplementationTestBasis extends PowerMockTestCase
 	}
 
 	/**
-	 * Puts all given handles into hash set.
+	 * Utility method. It can be used just for initialization set of handles in
+	 * one line. Puts all given handles into hash set.
 	 */
 	public static Set<HGPersistentHandle> set(
 			final HGPersistentHandle... handles)
@@ -199,9 +242,14 @@ public class BJEStorageImplementationTestBasis extends PowerMockTestCase
 	}
 
 	/**
-	 * Puts all handles which are accessible from given result set into hash
-	 * set. Result set is traverse from beginning to end one time.
+	 * Utility method. Puts all handles which are accessible from given result
+	 * set into hash set. In some test cases stored data returned as
+	 * {@link HGRandomAccessResult}. Two results cannot be compared directly. So
+	 * we put all handles into set and that compare two sets.
+	 *
 	 */
+	// TODO investigate if the handles is ordered from one call to another and
+	// return list of handles
 	public static Set<HGPersistentHandle> set(
 			final HGRandomAccessResult<HGPersistentHandle> handles)
 	{
