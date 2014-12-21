7f6959037f226b357ade6f15df3b186c20c7ff33
yuriysechko <yuriysechko@43784a7f-e236-0410-9472-c78ae7dffccf>
2014-12-21 10:11:18 +0000

    New test cases for BJEStorageImplementation.getIndex(String) added
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2970 43784a7f-e236-0410-9472-c78ae7dffccf

---
+++ storage/bdb-je/test/java/hgtest/storage/bje/BJEStorageImplementation/BJEStorageImplementation_getIndex_ByNameOnlyTest.java
@@ -0,0 +1,40 @@
+package hgtest.storage.bje.BJEStorageImplementation;
+
+import org.hypergraphdb.HGIndex;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
+
+public class BJEStorageImplementation_getIndex_ByNameOnlyTest extends
+		BJEStorageImplementationTestBasis
+{
+	@Test
+	public void indexNameIsNull() throws Exception
+	{
+		startup();
+		final String indexName = null;
+        HGIndex<Object,Object> retrievedIndex = storage.getIndex(indexName);
+        assertNull(retrievedIndex);
+        shutdown();
+	}
+
+    @Test
+    public void getNonStoredIndex() throws Exception {
+        startup();
+        final String indexName = "sample index";
+        HGIndex<Object, Object> retrievedIndex = storage.getIndex(indexName);
+        assertNull(retrievedIndex);
+        shutdown();
+    }
+
+    @Test
+    public void getIndexThatExists() throws Exception {
+        startup(1);
+        final String indexName = "sample index";
+        final HGIndex<Object, Object> storedIndex = storage.getIndex(indexName, null, null, null, true, true);
+        final HGIndex<Object, Object> retrievedIndex = storage.getIndex(indexName);
+        assertEquals(retrievedIndex, storedIndex);
+        shutdown();
+    }
+}
