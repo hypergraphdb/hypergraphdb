2d0d8a570a3dad3c1ee8759a2a5e1d035ad99ef3
yuriysechko <yuriysechko@43784a7f-e236-0410-9472-c78ae7dffccf>
2015-01-27 11:15:58 +0000

    new test cases for constructor in the KeyScanResultSet class
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@3016 43784a7f-e236-0410-9472-c78ae7dffccf

---
+++ storage/bdb-je/test/java/hgtest/storage/bje/KeyScanResultSet/KeyScanResultSet_constructorTest.java
@@ -0,0 +1,177 @@
+package hgtest.storage.bje.KeyScanResultSet;
+
+import com.sleepycat.je.*;
+import hgtest.storage.bje.TestUtils;
+import org.easymock.EasyMock;
+import org.hypergraphdb.HGException;
+import org.hypergraphdb.storage.BAUtils;
+import org.hypergraphdb.storage.ByteArrayConverter;
+import org.hypergraphdb.storage.bje.BJETxCursor;
+import org.hypergraphdb.storage.bje.KeyScanResultSet;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.testng.PowerMockTestCase;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * @author Yuriy Sechko
+ */
+@PrepareForTest(BJETxCursor.class)
+public class KeyScanResultSet_constructorTest extends PowerMockTestCase
+{
+	protected static final String DATABASE_NAME = "test_database";
+
+	protected final File envHome = TestUtils.createTempFile("IndexImpl",
+			"test_environment");
+	protected Environment environment;
+	protected Database database;
+	protected Transaction transaction;
+
+	protected void startupEnvironment() throws Exception
+	{
+		envHome.mkdir();
+		final EnvironmentConfig environmentConfig = new EnvironmentConfig();
+		environmentConfig.setAllowCreate(true).setReadOnly(false)
+				.setTransactional(true);
+		environment = new Environment(envHome, environmentConfig);
+		transaction = environment.beginTransaction(null, null);
+		final DatabaseConfig databaseConfig = new DatabaseConfig();
+		databaseConfig.setAllowCreate(true).setTransactional(true);
+		database = environment.openDatabase(transaction, DATABASE_NAME,
+				databaseConfig);
+	}
+
+	@BeforeMethod
+	public void resetMocksAndDeleteTestDirectory() throws Exception
+	{
+		PowerMock.resetAll();
+		TestUtils.deleteDirectory(envHome);
+		startupEnvironment();
+	}
+
+	@AfterMethod
+	public void verifyMocksAndDeleteTestDirectory() throws Exception
+	{
+		PowerMock.verifyAll();
+		transaction.commit();
+		database.close();
+		environment.close();
+		TestUtils.deleteDirectory(envHome);
+	}
+
+	@Test
+	public void cursorIsNull() throws Exception
+	{
+		final Exception expected = new HGException(
+				"java.lang.NullPointerException");
+
+		final DatabaseEntry keyIndex = new DatabaseEntry(new byte[] { 0, 0, 0,
+				0 });
+		final ByteArrayConverter<Integer> converter = new ByteArrayConverter<Integer>()
+		{
+			public byte[] toByteArray(final Integer input)
+			{
+				final byte[] buffer = new byte[4];
+				BAUtils.writeInt(input, buffer, 0);
+				return buffer;
+			}
+
+			public Integer fromByteArray(final byte[] byteArray,
+					final int offset, final int length)
+			{
+				return BAUtils.readInt(byteArray, 0);
+			}
+		};
+		PowerMock.replayAll();
+
+		try
+		{
+			new KeyScanResultSet<Integer>(null, keyIndex, converter);
+		}
+		catch (Exception occurred)
+		{
+			assertEquals(occurred.getClass(), expected.getClass());
+			assertEquals(occurred.getMessage(), expected.getMessage());
+		}
+	}
+
+	@Test
+	public void keyIndexIsNull() throws Exception
+	{
+		final ByteArrayConverter<Integer> converter = new ByteArrayConverter<Integer>()
+		{
+			public byte[] toByteArray(final Integer input)
+			{
+				final byte[] buffer = new byte[4];
+				BAUtils.writeInt(input, buffer, 0);
+				return buffer;
+			}
+
+			public Integer fromByteArray(final byte[] byteArray,
+					final int offset, final int length)
+			{
+				return BAUtils.readInt(byteArray, 0);
+			}
+		};
+		final Transaction transactionForTheRealCursor = environment
+				.beginTransaction(null, null);
+		final Cursor realCursor = database.openCursor(transaction, null);
+		realCursor.put(new DatabaseEntry(new byte[] { 0, 1, 2, 3 }),
+				new DatabaseEntry(new byte[] { 0, 1, 2, 3 }));
+		final BJETxCursor fakeCursor = PowerMock.createMock(BJETxCursor.class);
+		EasyMock.expect(fakeCursor.cursor()).andReturn(realCursor);
+		PowerMock.replayAll();
+
+		new KeyScanResultSet<Integer>(fakeCursor, null, converter);
+
+		realCursor.close();
+		transactionForTheRealCursor.commit();
+	}
+
+	@Test
+	public void bjeCursorThrowsException() throws Exception
+	{
+		final Exception expected = new HGException(
+				"java.lang.IllegalStateException: This exception is thrown by fake cursor.");
+
+		final DatabaseEntry keyIndex = new DatabaseEntry(new byte[] { 0, 0, 0,
+				0 });
+		final ByteArrayConverter<Integer> converter = new ByteArrayConverter<Integer>()
+		{
+			public byte[] toByteArray(final Integer input)
+			{
+				final byte[] buffer = new byte[4];
+				BAUtils.writeInt(input, buffer, 0);
+				return buffer;
+			}
+
+			public Integer fromByteArray(final byte[] byteArray,
+					final int offset, final int length)
+			{
+				return BAUtils.readInt(byteArray, 0);
+			}
+		};
+
+		final BJETxCursor fakeCursor = PowerMock.createMock(BJETxCursor.class);
+		EasyMock.expect(fakeCursor.cursor()).andThrow(
+				new IllegalStateException(
+						"This exception is thrown by fake cursor."));
+		PowerMock.replayAll();
+
+		try
+		{
+			new KeyScanResultSet<Integer>(fakeCursor, keyIndex, converter);
+		}
+		catch (Exception occurred)
+		{
+			assertEquals(occurred.getClass(), expected.getClass());
+			assertEquals(occurred.getMessage(), expected.getMessage());
+		}
+	}
+}
