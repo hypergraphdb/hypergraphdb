5ce21dd1c46b774486322c6807bc006b041c07a8
yuriysechko <yuriysechko@43784a7f-e236-0410-9472-c78ae7dffccf>
2015-02-10 15:19:15 +0000

    new test cases for DefaultIndexImpl class
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@3037 43784a7f-e236-0410-9472-c78ae7dffccf

---
+++ storage/bdb-je/test/java/hgtest/storage/bje/DefaultIndexImpl/DefaultIndexImpl_getComparatorTest.java
@@ -0,0 +1,108 @@
+package hgtest.storage.bje.DefaultIndexImpl;
+
+import com.sleepycat.je.Database;
+import com.sleepycat.je.DatabaseNotFoundException;
+import org.easymock.EasyMock;
+import org.hypergraphdb.HGException;
+import org.hypergraphdb.storage.bje.BJEConfig;
+import org.hypergraphdb.storage.bje.DefaultIndexImpl;
+import org.powermock.api.easymock.PowerMock;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Field;
+import java.util.Comparator;
+
+import static hgtest.storage.bje.TestUtils.assertExceptions;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
+
+/**
+ * @author Yuriy Sechko
+ */
+public class DefaultIndexImpl_getComparatorTest extends
+		DefaultIndexImplTestBasis
+{
+	@Test
+	public void indexIsNotOpened() throws Exception
+	{
+		final Exception expected = new NullPointerException();
+
+		PowerMock.replayAll();
+		final DefaultIndexImpl indexImpl = new DefaultIndexImpl(null, storage,
+				transactionManager, keyConverter, valueConverter, comparator);
+
+		try
+		{
+			indexImpl.getComparator();
+		}
+		catch (Exception occurred)
+		{
+			assertExceptions(occurred, expected);
+		}
+		finally
+		{
+			closeDatabase(indexImpl);
+		}
+	}
+
+	@Test
+	public void indexIsOpened() throws Exception
+	{
+		mockStorage();
+		PowerMock.replayAll();
+		final DefaultIndexImpl indexImpl = new DefaultIndexImpl(INDEX_NAME,
+				storage, transactionManager, keyConverter, valueConverter,
+				comparator);
+		indexImpl.open();
+
+		final Comparator<byte[]> actualComparator = indexImpl.getComparator();
+
+		assertNull(actualComparator);
+		closeDatabase(indexImpl);
+	}
+
+	@Test
+	public void databaseThrowsException() throws Exception
+	{
+		final Exception expected = new HGException(
+				"com.sleepycat.je.DatabaseNotFoundException: (JE 5.0.34) This exception is thrown by fake database.");
+
+		mockStorage();
+		Database fakeDatabase = PowerMock.createStrictMock(Database.class);
+		EasyMock.expect(fakeDatabase.getConfig()).andThrow(
+				new DatabaseNotFoundException(
+						"This exception is thrown by fake database."));
+		PowerMock.replayAll();
+		final DefaultIndexImpl indexImpl = new DefaultIndexImpl(INDEX_NAME,
+				storage, transactionManager, keyConverter, valueConverter,
+				comparator);
+		indexImpl.open();
+
+		// inject fake database and imitate error
+		final Field databaseField = indexImpl.getClass().getDeclaredField(
+				DATABASE_FIELD_NAME);
+		databaseField.setAccessible(true);
+		final Database realDatabase = (Database) databaseField.get(indexImpl);
+		databaseField.set(indexImpl, fakeDatabase);
+
+		// call method which is under test
+		try
+		{
+			indexImpl.getComparator();
+		}
+		catch (Exception occurred)
+		{
+			assertExceptions(occurred, expected);
+		}
+		finally
+		{
+			// set 'null' for reference to fake database
+			// to make doubly sure that it will not be reused as mock
+			fakeDatabase = null;
+			// inject real database again
+			databaseField.set(indexImpl, realDatabase);
+			// close database like in other test cases
+			closeDatabase(indexImpl);
+		}
+	}
+}
