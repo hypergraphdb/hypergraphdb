288701d9704055b9e6aa0822e0b3bfe00d0a873f
YuriySechko@gmail.com <YuriySechko@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2015-04-10 13:46:28 +0000

    javadoc updated
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@3077 43784a7f-e236-0410-9472-c78ae7dffccf

---
+++ testcore/test/java/hgtest/storage/HGSortIndex/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains common tests for BJE DefaultIndexImpl, BDB DefaultIndexImpl. 
+ * <p>
+ * All this classes implement HGSortIndex interface.
+ */
+package hgtest.storage.HGSortIndex;
